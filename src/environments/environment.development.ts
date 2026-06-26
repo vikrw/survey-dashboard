09c69aee91bdf3dbb487aa7aeccb0df586d1b07b
vikas kumar <vkumar589@gmail.com>
2026-06-26 20:11:08 +0530

    env files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDo_vJ3D4PMXLvwn_kfsjAKV2ZXYljl8WI",
+    authDomain: "survey-dashboard-8257f.firebaseapp.com",
+    projectId: "survey-dashboard-8257f",
+    storageBucket: "survey-dashboard-8257f.firebasestorage.app",
+    messagingSenderId: "278214124264",
+    appId: "1:278214124264:web:e948b9ce3433b8ded7df5d",
+    measurementId: "G-ZVKVYMQWDW"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDo_vJ3D4PMXLvwn_kfsjAKV2ZXYljl8WI",
+    authDomain: "survey-dashboard-8257f.firebaseapp.com",
+    projectId: "survey-dashboard-8257f",
+    storageBucket: "survey-dashboard-8257f.firebasestorage.app",
+    messagingSenderId: "278214124264",
+    appId: "1:278214124264:web:e948b9ce3433b8ded7df5d",
+    measurementId: "G-ZVKVYMQWDW",
+    databaseURL: "https://survey-dashboard-8257f-default-rtdb.firebaseio.com/"
+  }
+};
